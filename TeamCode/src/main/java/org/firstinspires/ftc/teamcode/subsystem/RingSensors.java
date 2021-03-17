511996bd17fa138566e0b1ec4610f082d2457c18
Lucas <littleninjaleo@gmail.com>
2021-03-16 18:19:38 -0700

    changed RING_WAIT

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/RingSensors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/RingSensors.java
@@ -52,7 +52,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
  */
 public class RingSensors {
 
-    private static final double RING_WAIT = 100;
+    private static final double RING_WAIT = 500;
     public RingSensors(Telemetry telemetry, HardwareMap hardwareMap) {
         this.hardwareMap = hardwareMap;
         this.telemetry = telemetry;
