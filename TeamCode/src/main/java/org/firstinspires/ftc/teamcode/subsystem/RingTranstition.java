c97b88eb8cb747de13a6dff393e6b5a6473e196e
Lucas <littleninjaleo@gmail.com>
2021-04-02 19:41:37 -0700

    chnaged intake speed and ring wait

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/RingTranstition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/RingTranstition.java
@@ -57,8 +57,8 @@ public class RingTranstition {
     private static final double SHOOTING_UPPER_BELTS_SPEED = -1250.0;
     private static final double SHOOTING_LOWER_BELTS_SPEED = 750.0;
 
-    private static final double INTAKE_UPPER_BELTS_SPEED = -500.0;
-    private static final double LOWER_BELT_INTAKE_SPEED = 500.0;
+    private static final double INTAKE_UPPER_BELTS_SPEED = -750.0;
+    private static final double LOWER_BELT_INTAKE_SPEED = 750.0;
 
     private static final double ADVANCING_UPPER_BELTS_SPEED = -1250.0*0.25;
     private static final double LOWER_BELT_ADVANCING_SPEED = 750.0*0.25;
